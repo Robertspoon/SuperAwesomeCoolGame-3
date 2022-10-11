393993834dde5e4e4d7c4d331095659c6b56eb06
Robertspoon <robertthemcjug@gmail.com>
2022-10-11 10:03:15 -0300

    dwadawwawwa

--- SuperAwesomeCoolGame-3/Program.cs
+++ SuperAwesomeCoolGame-3/Program.cs
@@ -8,8 +8,50 @@ namespace SuperAwesomeCoolGame_3
 {
     internal class Program
     {
+        static int health;
+        static int shield;
+        static int score;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("<-------------------------------------->");
+            Console.WriteLine("THE SUPER AWESOME COOL GAME THE THIRD!!!");
+            Console.WriteLine("<-------------------------------------->");
+            Console.WriteLine();
+
+            health = 100;
+            score = 0;
+            shield = 100;
+
+            ShowHUD();
+
+            Console.ReadKey();
+        }
+        static void ShowHUD()
+        {
+            Console.WriteLine("<-------------->");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("<-------------->");
+
+
+        }
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+            if (health <0)
+            {
+                health = 0;
+            }
+        }
+        static void Heal(int heal)
+        {
+            health = health + heal;
+            if (health >100)
+            {
+                health = 100;
+            }
         }
     }
 }
